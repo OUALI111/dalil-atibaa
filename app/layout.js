3fe5bc41c9caf5e2038542a75103b6424b2798ed
Nasri <TELECOM1234JMAI.COM@GMAIL.COM>
2026-04-27 15:20:21 +0200

    feat: google search console verification + metadataBase

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
 import './globals.css'
 
 export const metadata = {
-  metadataBase: new URL('https://dalil-atibaa.vercel.app'),
+  metadataBase: new URL('https://www.dalil-atibaa.com'),
+  verification: {
+    google: 'D547io5wtOUcjrJPy8ssvzkAUg0RTYeibFx7jfKc03w',
+  },
 }
 
 export default function RootLayout({ children }) {
