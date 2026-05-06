91b5d5c8439dcb13cc6627fd66724d903c40156d
Nasri <TELECOM1234JMAI.COM@GMAIL.COM>
2026-05-07 00:02:20 +0200

    feat: Google Analytics 4 G-3DJLB4FLQC

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3DJLB4FLQC"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3DJLB4FLQC');
+          `
+        }} />
+      </head>
       <body>{children}</body>
     </html>
   )
