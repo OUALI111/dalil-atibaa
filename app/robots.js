713dac0f727487b4291d9ca1fd4eca177f97467a
Nasri <TELECOM1234JMAI.COM@GMAIL.COM>
2026-07-03 16:29:46 +0200

    fix(seo): robots.txt - remove duplicate User-Agent block that was blocking entire site with Disallow: /

--- app/robots.js
+++ app/robots.js
@@ -6,11 +6,6 @@ export default function robots() {
         allow: '/',
         disallow: ['/admin/'],
       },
-      {
-        userAgent: '*',
-        disallow: '/',
-        host: 'dalil-atibaa.vercel.app',
-      },
     ],
     sitemap: 'https://www.dalil-atibaa.com/sitemap.xml',
     host: 'https://www.dalil-atibaa.com',
